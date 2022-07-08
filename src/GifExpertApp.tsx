3284b4b3a46f7618604c7e5baff39d16ad6b42ea
BastigolAS7 <bastyas9@gmail.com>
2022-07-08 19:27:48 -0400

    keycloak && languages test

--- src/GifExpertApp.tsx
+++ src/GifExpertApp.tsx
@@ -1,13 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AddCategory } from './Components/AddCategory';
 import { GifGrid } from './Components/GifGrid';
+import { keycloak } from './config/keycloak';
+import { useTranslation } from 'react-i18next';
 
 const GifExpertApp = () => {
   const [categories, setcategories] = useState(['Dragon Ball Z']);
+  const [accessToken, setAccessToken] = useState('');
+  const [t, i18n] = useTranslation('global');
+
+  useEffect(() => {
+    keycloak.init({ onLoad: 'login-required' }).then((authenticated) => {
+      if (authenticated) {
+        if (keycloak.token) {
+          setAccessToken(keycloak.token);
+        }
+      }
+    });
+  }, []);
 
   return (
     <div>
+      <p>{t('header.hello')}, Bastián</p>
+      <button onClick={() => i18n.changeLanguage('es')}>ES</button>
+      <button onClick={() => i18n.changeLanguage('en')}>EN</button>
       <h2>GifExpertApp</h2>
+      <p>{accessToken}</p>
       <AddCategory setCategories={setcategories} />
       <hr />
       <ol>
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,33 @@ import ReactDOM from 'react-dom/client';
 import GifExpertApp from './GifExpertApp';
 import './index.css';
 import 'animate.css';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+
+import global_en from './translation/en/global.json';
+import global_es from './translation/es/global.json';
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'en',
+  resources: {
+    es: {
+      global: global_es,
+    },
+    en: {
+      global: global_en,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<GifExpertApp />);
+root.render(
+  <I18nextProvider i18n={i18next}>
+    <GifExpertApp />
+  </I18nextProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
